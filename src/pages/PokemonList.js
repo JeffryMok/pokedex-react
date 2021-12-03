656125bbab9fc73fc25b2ce3552f7da9fc1ae1c6
Jeffry17 <jeffry.mok@dansmultipro.com>
2021-12-03 16:53:14 +0700

    Testing emotion library

--- src/pages/PokemonList.js
+++ src/pages/PokemonList.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { css } from '@emotion/css';
 
 const PokemonList = () => {
   const navigate = useNavigate();
+  const color = 'red';
   return (
-    <div>
+    <div
+      className={css`
+        padding: 32px;
+        font-size: 16px;
+        font-weight: 600;
+        &:hover {
+          color: ${color};
+        }
+      `}
+    >
       <div>Pokemon List</div>
       <button onClick={() => navigate('/detail/1')}>
         Ke Detail
