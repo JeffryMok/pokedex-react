5387d6f067e3424f856de3c009b5c05c0ec53200
Jeffry17 <jeffry.mok@dansmultipro.com>
2021-12-04 17:34:06 +0700

    Modify Pokemon Detail page

--- src/pages/PokemonDetail.js
+++ src/pages/PokemonDetail.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { gql, useQuery } from '@apollo/client';
+import { css } from '@emotion/css';
+import { Accordion, AccordionDetails, AccordionSummary, Button, IconButton } from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
 const PokemonDetail = () => {
   const navigate = useNavigate();
@@ -11,6 +15,9 @@ const PokemonDetail = () => {
       pokemon(name: $name) {
         id
         name
+        sprites {
+          front_default
+        }
         abilities {
           ability {
             name
@@ -26,28 +33,79 @@ const PokemonDetail = () => {
             name
           }
         }
-        message
-        status
       }
     }
   `;
 
   const gqlVariables = { name: params.name };
 
-  const { loading, error, data } = useQuery(GET_POKEMON_DETAIL, { variables: gqlVariables });
+  const {
+    loading,
+    error,
+    data: {
+      pokemon: {
+        name,
+        sprites,
+        abilities,
+        moves,
+        types,
+      } = {},
+    } = {},
+  } = useQuery(GET_POKEMON_DETAIL, { variables: gqlVariables });
   if (loading) return <div>Loading...</div>
   if (error) return <div>Error: {error.message}</div>
-  console.log(data);
+
+  const normalizeName = (word) => {
+    return word.replace(/-/g, " ");
+  }
 
   return (
-    <div>
-      <div>Pokemon Detail</div>
-      <div>{data.pokemon.name}</div>
-      <div>{data.pokemon.abilities.map((elm) => (
-        <p>{elm.ability.name}</p>
-      ))}</div>
-      <button onClick={() => navigate('/')}>Balik ke list</button>
-      <button onClick={() => navigate('/my-list')}>Ke MyList</button>
+    <div className={css`padding: 8px; font-size: 14px; text-align: center; text-transform: capitalize`}>
+      <div className={css`text-align: left;`}>
+        <IconButton onClick={() => navigate(-1)}>
+          <ArrowBackIcon fontSize="small" />
+          <span className={css`margin-left: 4px; font-size: 14px`}>Back</span>
+        </IconButton>
+      </div>
+      <div className={css`font-size: 24px; font-weight: 600`}>Pokemon Detail</div>
+      <div>{name}</div>
+      <div>
+        <img src={sprites?.front_default} alt={name} />
+      </div>
+      <div className={css`display: flex; justify-content: space-evenly; text-transform: uppercase`}>
+        {types.map((elm) => (
+          <div>{elm.type.name}</div>
+        ))}
+      </div>
+      <div className={css`text-align: left`}>
+        <Accordion>
+          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+            <div>Abilities</div>
+          </AccordionSummary>
+          <AccordionDetails>
+            <ul>
+              {abilities.map((elm) => (
+                <li>{normalizeName(elm.ability.name)}</li>
+              ))}
+            </ul>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion>
+          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+            <div>Moves</div>
+          </AccordionSummary>
+          <AccordionDetails>
+            <ul>
+              {moves.map((elm) => (
+                <li>{normalizeName(elm.move.name)}</li>
+              ))}
+            </ul>
+          </AccordionDetails>
+        </Accordion>
+      </div>
+      <div className={css`margin-top: 8px`}>
+        <Button variant="contained">Catch</Button>
+      </div>
     </div>
   )
 }
