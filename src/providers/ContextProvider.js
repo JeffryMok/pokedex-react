17c397fa61521516ab3ad305de4271363ab08b1a
Jeffry17 <jeffry.mok@dansmultipro.com>
2021-12-05 15:30:11 +0700

    Add presistance for context

--- src/providers/ContextProvider.js
+++ src/providers/ContextProvider.js
@@ -1,15 +1,18 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 
 export const PokemonContext = createContext({});
 
 export const ContextProvider = ({ children }) => {
-  const [myPokemonList, setMyPokemonList] = useState([]);
-  const [totalOwned, setTotalOwned] = useState(0);
+  const storedData = JSON.parse(localStorage.getItem("my-pokemon-list"));
+  const [myPokemonList, setMyPokemonList] = useState(storedData || []);
+
+  useEffect(() => {
+    localStorage.setItem("my-pokemon-list", JSON.stringify(myPokemonList));
+  }, [myPokemonList]);
 
   const addNewPokemon = (pokemon) => {
     const tempList = [...myPokemonList];
-    tempList.push(pokemon);
-    setTotalOwned(myPokemonList+1);
+    tempList.push({ ...pokemon, id: myPokemonList.length + 1 });
     setMyPokemonList(tempList);
   }
 
@@ -17,7 +20,6 @@ export const ContextProvider = ({ children }) => {
     <PokemonContext.Provider
       value={{
         myPokemonList,
-        totalOwned,
         addNewPokemon,
       }}
     >
